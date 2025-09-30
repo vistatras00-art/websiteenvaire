1ccfb9ac7f2851d5295fcf1fa64d5cba44baa79b
vistatras00-art <vistatras00@gmail.com>
2025-09-30 19:02:45 +0200

    Updated build-manifest.json

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"components/booking-modal.tsx -> @/components/ui/calendar\":{\"id\":\"components/booking-modal.tsx -> @/components/ui/calendar\",\"files\":[\"static/chunks/_app-pages-browser_components_ui_calendar_tsx.js\"]}}"
